c7297ee1835a743e525cd78d47b0f2c87ac1c11b
andrei1058 <andrew.dascalu@gmail.com>
2016-12-11 18:23:50 +0100

    Metoda cu arraylist blocks, dar nu stiu cum sa fac get

--- src/com/andrei1058/ageofempire/locations/StructureBuilder.java
+++ src/com/andrei1058/ageofempire/locations/StructureBuilder.java
@@ -14,19 +14,11 @@ import static com.andrei1058.ageofempire.Main.plugin;
 
 public class StructureBuilder {
 
-    public ArrayList<Location> locatii = new ArrayList<>();
-    public HashMap<Location, Material> materiale = new HashMap<>();
-
-    /**
-     * Get all blocks between two points and return a 3d array
-     */
-
-    public int[][][] getStructure(Block block, Block block2) {
+    public ArrayList<Block> getStructure(Block block, Block block2) {
+        ArrayList<Block> blocks = new ArrayList<>();
         int minX, minZ, minY;
         int maxX, maxZ, maxY;
 
-
-        //Couldv'e used Math.min()/Math.max(), but that didn't occur to me until after I finished this :D
         minX = block.getX() < block2.getX() ? block.getX() : block2.getX();
         minZ = block.getZ() < block2.getZ() ? block.getZ() : block2.getZ();
         minY = block.getY() < block2.getY() ? block.getY() : block2.getY();
@@ -35,52 +27,27 @@ public class StructureBuilder {
         maxZ = block.getZ() > block2.getZ() ? block.getZ() : block2.getZ();
         maxY = block.getY() > block2.getY() ? block.getY() : block2.getY();
 
-        int[][][] blocks = new int[maxX - minX + 1][maxY - minY + 1][maxZ - minZ + 1];
-
         for (int x = minX; x <= maxX; x++) {
-
             for (int y = minY; y <= maxY; y++) {
-
                 for (int z = minZ; z <= maxZ; z++) {
-
                     Block b = block.getWorld().getBlockAt(x, y, z);
-                    blocks[x - minX][y - minY][z - minZ] = b.getTypeId();
-                }
-            }
-        }
-
-        return blocks;
-
-    }
-
-
-    /**
-     * Pastes a structure to a desired location
-     */
-
-    public void paste(int[][][] blocks, Location l) {
-        for (int x = 0; x < blocks.length; x++) {
-            for (int y = 0; y < blocks[x].length; y++) {
-                for (int z = 0; z < blocks[x][y].length; z++) {
-                    Location neww = l.clone();
-                    neww.add(x, y, z);
-                    if (blocks[x][y][z] != 0) {
-                        if (Material.getMaterial(blocks[x][y][z]) != Material.AIR) {
-                            locatii.add(neww);
-                            materiale.put(neww, Material.getMaterial(blocks[x][y][z]));
-                        }
+                    if (b.getType() != Material.AIR) {
+                        blocks.add(b);
                     }
                 }
-
             }
         }
-        final List<Location> orderedLocation = new ArrayList<>();
+        return blocks;
+    }
 
-        orderedLocation.addAll(locatii);
+    public void paste(ArrayList blocks, Location l) {
+        final List<Block> orderedLocation = new ArrayList<>();
 
-        Collections.sort(orderedLocation, new Comparator<Location>() {
+        orderedLocation.addAll(blocks);
+
+        Collections.sort(orderedLocation, new Comparator<Block>() {
             @Override
-            public int compare(Location block1, Location block2) {
+            public int compare(Block block1, Block block2) {
                 return Double.compare(block1.getY(), block2.getY());
             }
         });
@@ -88,7 +55,7 @@ public class StructureBuilder {
         Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
             @Override
             public void run() {
-                final int size = locatii.size();
+                final int size = blocks.size();
                 final int blocksPerTime = 1;
                 final long delay = 0L;
 
@@ -99,14 +66,12 @@ public class StructureBuilder {
                         @Override
                         public void run() {
                             for (int i = 0; i < blocksPerTime; i++) {
-                                if (index < size) {
-                                    Block block = orderedLocation.get(index).getBlock();
-                                    Material mat = materiale.get(orderedLocation.get(index));
-                                    if (mat != Material.AIR) {
-                                        block.setType(mat);
-                                        block.getState().update(true);
-                                        block.getWorld().playEffect(block.getLocation(), Effect.STEP_SOUND, block.getTypeId());
-                                    }
+                                if (index <= size) {
+                                    Block block = orderedLocation.get(index);
+                                    block.getWorld().getBlockAt(block.getLocation()).setType(block.getType());
+                                    block.getState().setData(block.getState().getData());
+                                    block.getState().update(true);
+                                    block.getWorld().playEffect(block.getLocation(), Effect.STEP_SOUND, block.getTypeId());
                                     index += 1;
                                 } else {
                                     this.cancel();
@@ -119,45 +84,6 @@ public class StructureBuilder {
             }
         }, 100L);
     }
-
-    /**
-     * Save a structure with a desired name
-     */
-
-    public void save(String name, int[][][] b) {
-        ObjectOutputStream oos = null;
-        FileOutputStream fout = null;
-
-        File f = new File(plugin.getDataFolder() + "/schematics/" + name + ".schem");
-        File dir = new File(plugin.getDataFolder() + "/schematics");
-
-        try {
-            dir.mkdirs();
-            f.createNewFile();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
-
-        try {
-            fout = new FileOutputStream(f);
-            oos = new ObjectOutputStream(fout);
-            oos.writeObject(b);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (oos != null) {
-                try {
-                    oos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    /**
-     * Load structure from file
-     */
 
     public int[][][] load(String name) {
 
